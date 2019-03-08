a7e3257ac51cdbdd57b7819f17d63ed82158412f
Guy Lissak <lissakg@gmail.com>
2019-03-08 18:14:22 +0200

    adding to project

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+//git init - to init the repository
+//npm init - creates the json package
+//npm install -g live-server
+var _ = require('lodash');
+//https://lodash.com
+//originally lodash was created for Node.js but since we installed the browserify we can use it on javascript!
+/*  "scripts": {
+    "build": "browserify script.js > bundle.js"
+  },*/
+  //adding the above to package.json will build the bundle with a script that can be
+  //run from the terminal - npn run build
+var array = [1,2,3,4,5,6,7,8];
+console.log('answer:', _.without(array,3));
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
